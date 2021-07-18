7f0145c3766d64bab8a9fa6e07ce2d1dd69fd3f5
Michael C <michael@mchang.name>
2021-07-18 03:21:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// dotenv
+require('dotenv').config()
+
+// dayjs
+var dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+// discord.js
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.TOKEN);
+
+const getTimeEmoji = (now) => {
+  console.log(now.hour());
+  if (now.hour() === 0 || now.hour() === 12) {
+    return '🕛';
+  } else if (now.hour() === 1 || now.hour() === 13) {
+    return '🕐';
+  } else if (now.hour() === 2 || now.hour() === 14) {
+    return '🕑';
+  } else if (now.hour() === 3 || now.hour() === 15) {
+    return '🕒';
+  } else if (now.hour() === 4 || now.hour() === 16) {
+    return '🕓';
+  } else if (now.hour() === 5 || now.hour() === 17) {
+    return '🕔';
+  } else if (now.hour() === 6 || now.hour() === 18) {
+    return '🕕';
+  } else if (now.hour() === 7 || now.hour() === 19) {
+    return '🕖';
+  } else if (now.hour() === 8 || now.hour() === 20) {
+    return '🕗';
+  } else if (now.hour() === 9 || now.hour() === 21) {
+    return '🕘';
+  } else if (now.hour() === 10 || now.hour() === 22) {
+    return '🕙';
+  } else if (now.hour() === 11 || now.hour() === 23) {
+    return '🕚';
+  } else {
+    return ''
+  }
+}
+
+const getTime = (time, zone) => `${getTimeEmoji(time)} ${time.format('HH:mm')} ${zone}`
+
+async function getBothTimes() {
+  const now = dayjs()
+  const time1 = dayjs(now).tz(process.env.TZ_ONE)
+  const time2 = dayjs(now).tz(process.env.TZ_TWO)
+  return `${getTime(time1, process.env.TZ_ONE_NAME)}•${getTime(time2, process.env.TZ_TWO_NAME)}`
+}
+
+function renameChannel(client, channel, data) {
+  client.channels.cache.get(channel).setName()
+    .then(channel => channel.setName(data))
+}
+
+client.on('ready', () => {
+  console.log("Ready")
+  setInterval(function(){client.emit('customClockUpdate')},600000)
+});
+client.on('customClockUpdate', () => {
+  const data = getBothTimes()
+  renameChannel(client, process.env.CHANNELID, data)
+})
